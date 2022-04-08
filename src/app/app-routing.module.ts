2704feb20a56970ad42badbeb5564d3da01145ba
emizael <erikson_mizael@hotmail.com>
2022-04-08 14:17:28 -0300

    ajustes inicio jogo da cobra

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'jogo-cobrinha', loadChildren: () => import('./models/jogo-cobrinha/jogo-cobrinha.module').then(m => m.JogoCobrinhaModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

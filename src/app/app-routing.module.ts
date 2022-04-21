413712ea71d11f8981cda82265fb226334b37965
emizael <erikson_mizael@hotmail.com>
2022-04-21 12:54:47 -0300

    Adicionando  o menu flutuante

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'jogo-cobrinha', loadChildren: () => import('./models/jogo-cobrinha/jogo-cobrinha.module').then(m => m.JogoCobrinhaModule) }];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
-import { MenuItem } from 'primeng/api';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
   title = 'linkedin';
-  items!: MenuItem[];
   display: boolean = false;
+  bola = document.getElementsByClassName('opcao-flutuante') as HTMLCollectionOf<HTMLElement>;
+  clicado: boolean = false;
 
-
-  constructor(private primengConfig: PrimeNGConfig) { }
-
-  ngOnInit() {
+  constructor(private primengConfig: PrimeNGConfig) {
     this.primengConfig.ripple = true;
+  }
+  ngOnInit(): void {
 
-    this.items = [{
-      label: 'File',
-      items: [
-        { label: 'New', icon: 'pi pi-fw pi-plus' },
-        { label: 'Download', icon: 'pi pi-fw pi-download' }
-      ]
-    },
-    {
-      label: 'Edit',
-      items: [
-        { label: 'Add User', icon: 'pi pi-fw pi-user-plus' },
-        { label: 'Remove User', icon: 'pi pi-fw pi-user-minus' }
-      ]
-    }];
+  }
 
+  clck() {
+    document.addEventListener('mousemove', (event) => {
+      console.log(`Mouse X: ${event.clientX}, Mouse Y: ${event.clientY}`);
+      const x = event.clientX;
+      const y = event.clientY
+      console.log(this.clicado);
+      if (this.clicado) {
+        this.bola[0].style.left = x - 30 + "px";
+        this.bola[0].style.top = y -30 + "px";
+      }
+    });
+  }
+
+  mudarBola(event: PointerEvent, a: boolean): void {
+    console.log(a);
+    console.log(event.x);
   }
 
   mostrar_modal() {
